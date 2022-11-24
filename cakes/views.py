f0fd6c2efcd63ed437fed9afb00439443016c8f6
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-11-24 12:32:31 +0300

    get cake elements from db

--- cakes/views.py
+++ cakes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import CakeSize, CakeForm, CakeTopping, CakeBerry, CakeDecor
 
 
 def index(request):
-    return render(request, 'index.html')
+    cake_elements = {
+        'sizes': CakeSize.objects.all(),
+        'forms': CakeForm.objects.all(),
+        'toppings': CakeTopping.objects.all(),
+        'berries': CakeBerry.objects.all(),
+        'decors': CakeDecor.objects.all()
+    }
+    return render(request, template_name='index.html', context={
+        'cake_elements': cake_elements
+    })
 
 
 def view_lk_order(request):
